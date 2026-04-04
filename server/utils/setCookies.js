b21a9d747f5f10ad1da7085cae8e83c81f437da5
shivcodecf <shivam2113128@akgec.ac.in>
2026-04-05 05:13:50 +0530

    cors fix

--- server/utils/setCookies.js
+++ server/utils/setCookies.js
@@ -1,13 +1,11 @@
 export const setCookies = (token, res) => {
- const isProd = process.env.NODE_ENV === "production";
+  const isProd = process.env.NODE_ENV === "production";
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? "none" : "lax", 
+    secure: true, // 🔥 ALWAYS true in deployed env
+    sameSite: "none", // 🔥 REQUIRED for cross-origin
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
-
-
